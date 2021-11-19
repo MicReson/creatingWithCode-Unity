be169f9873ec2fdc97e85082625352e9b8e773f8
MicReson <92884357+MicReson@users.noreply.github.com>
2021-11-19 12:26:40 -0500

    Challenge-1 Done And Update Git ignore
    Challenge 1 is done and git ignore has been updated with a git ignore unity template

---
+++ Create with Code/Challenge-1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+    private GameObject propellerObject;
+
+    void FixedUpdate()
+    {
+        propellerObject = GameObject.Find("Propeller");
+
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed* Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left * verticalInput * rotationSpeed * Time.deltaTime);
+
+        propellerObject.transform.Rotate(0.0f, 0.0f, rotationSpeed);
+    }
+}
