2362676d88503c7825505748ddb8ee18fe1e5c2a
MicReson <92884357+MicReson@users.noreply.github.com>
2021-11-19 13:49:26 -0500

    Challenge 2 Done
    Challenge 2 completed

--- Create with Code/Challenge-2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create with Code/Challenge-2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool coolDown = true;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (coolDown)
+            {
+                dogSpawn();
+                StartCoroutine(noSpawnObject(1.0f));
+
+            }
         }
+  
+    }
+
+    void dogSpawn()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+
+    IEnumerator noSpawnObject(float seconds) //Coroutine to avoid spamming the spacebar
+    {
+        coolDown = false;
+        yield return new WaitForSeconds(seconds);
+        coolDown = true;
     }
 }
--- Create with Code/Challenge-2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Create with Code/Challenge-2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,12 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    //private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(1.5f, 4.0f));
     }
 
     // Spawn random ball at random x position at top of play area
